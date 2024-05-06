a2eded5d2393bbc15a5623c944bb9ccc9f37e64c
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-05-06 22:00:30 +0500

    Add files via upload

---
+++ new.ts
@@ -0,0 +1,31 @@
+import { differenceInSeconds } from "date-fns"
+
+function* countdownTimer(second: number) {
+    while (second > 0) {
+        yield second;
+        second--;
+    }
+}
+
+
+let timerIterator = countdownTimer(10);
+
+function displayCountdown() {
+    let result = timerIterator.next();
+
+    if (!result.done) {
+        const now = new Date();
+
+        const countdownTimer = new Date(now.getTime() + (result.value * 1000))
+
+        const remainingSeconds = differenceInSeconds(countdownTimer, now)
+
+        console.log(`Remaining Seconds: ${remainingSeconds}`)
+
+        setTimeout(displayCountdown, 1000)
+    } else {
+        console.log("CountDown Complete!")
+    }
+}
+
+displayCountdown()
